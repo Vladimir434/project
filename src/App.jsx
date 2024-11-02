4e83606d93818687d4f750b7aa39bf466558f475
Vladimir434 <korolevskijvladimir73@gmail.com>
2024-11-02 15:31:49 +0600

    lesson

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  let name = 'ivan';
+  console.log(name);
+
+  return(
+    <div>
+    <h1>Lorem ipsum dolor sit.</h1>
+    <h2>hallo {name}</h2>
+    </div>
+  )
+  
+}
+
+export default App
