c55011fa305e0ebcbe86d71d6e3c9c69c5f89676
Vladimir434 <korolevskijvladimir73@gmail.com>
2024-11-02 17:37:21 +0600

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import './App.css'
+import "./App.css";
+import Hesder from "./components/header/hesder";
 
 function App() {
-  let name = 'ivan';
-  console.log(name);
+  // let name = "ivan";
+  // console.log(name);
 
-  return(
-    <div>
-    <h1>Lorem ipsum dolor sit.</h1>
-    <h2>hallo {name}</h2>
-    </div>
-  )
-  
+  // const getName = (isAdmin) => {
+  //   if (isAdmin === true) {
+  //     return "привет админ";
+  //   } else {
+  //     return "ты не вдмин";
+  //   }
+  // };
+  // const arrayName = [{name:'Ivan',},{name:'Eric'}];
+  // const onHandleClick = (id) =>{
+  //   console.log(id);
+  // }
+
+  const isAdmin = true;
+
+  return (
+    <>
+      <div>
+        <Hesder />
+      </div>
+      <div>
+        {/* {isAdmin === true ? <h1>Hello Admin</h1> : <h1>Hello user</h1>} */}
+        {isAdmin && <h1>Hello admon</h1>}
+        {!isAdmin && <h1>Hello user</h1>}
+      </div>
+      {/* <h2>{getName(true)}</h2> */}
+      {/* <div>
+        <button className="btn" onClick={() => onHandleClick(1323)}>click mi</button>
+      </div> */}
+      {/* <div>
+        { {arrayName.map((item,index) =>(
+          <p key={index}>{item.name}</p>
+        ))}}
+      </div> */}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/header/hesder.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+const Hesder = () => {
+  const logo = "vladimir"
+  return (
+    <header className='header'>
+      <h1>{logo}</h1>
+    </header>
+  )
+}
+
+export default Hesder
